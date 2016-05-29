eab0de0b074b1556ecb3d29218122271f48b73fc
Anne Johnson <annekjohnson23@yahoo.com>
2016-05-29 00:08:23 -0400

    Remove unneeded shared example

--- spec/support/shared_examples/tweet.rb
+++
@@ -1,7 +0,0 @@
-shared_examples 'a tweet' do
-  let(:max_num_characters) { 140 }
-
-  it 'contains no more than the allowed number of characters' do
-    expect(tweet.length <= max_num_characters).to be_truthy
-  end
-end
